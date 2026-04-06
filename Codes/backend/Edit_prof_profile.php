1e72ad83f7a38b0d7f181e6263c3d4c1feb34425
CEO-vs <charbel.otk@gmail.com>
2026-04-06 12:32:28 +0300

    Added some restrictions to the edit

--- Codes/backend/Edit_prof_profile.php
+++ Codes/backend/Edit_prof_profile.php
@@ -2,10 +2,42 @@
     session_start();
     include("database.php");
 
+     $ulDomain = "ul.edu.lb";
+    $arabicPattern = "/^[\x{0600}-\x{06FF}\s]+$/u";
+    $passwordFormat = "/^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/";
+
     if (!isset($_SESSION["email"])) {
         header("location: login.html");
         exit();
     }
+
+ //Checking the first and last name if they are written in arabic
+        if (!preg_match($arabicPattern, $firstName)) {
+            $errors[] = "First name must be in Arabic.";
+        }
+        if (!preg_match($arabicPattern, $lastName)) {
+            $errors[] = "Last name must be in Arabic.";
+        }
+
+         if(value_exists($conn,"prof_phone",$phone)){
+            $errors[] = "Phone is already taken";
+        }
+
+        //Checking the file number
+        if (empty($fileNumber) || !filter_var($fileNumber, FILTER_VALIDATE_INT)) {
+            $errors[] = "File number is required and must be a number.";
+        }
+        if(value_exists($conn,"prof_file_nb",$fileNumber)){
+            $errors[] = "This file number is already used";
+        }
+
+        //Checking the email
+        if(!filter_var($email,FILTER_VALIDATE_EMAIL) || substr($email, -strlen($ulDomain)) != $ulDomain){
+            $errors[] = "Invalid email format";
+        }
+        if(value_exists($conn,"prof_email",$email)){
+            $errors[] = "This email is already used";
+        }
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_SESSION["email"];
