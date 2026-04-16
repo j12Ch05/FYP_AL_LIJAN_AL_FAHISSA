1765831a9cc332300bbfb69c275d5ee0d7c077c0
CEO-vs <charbel.otk@gmail.com>
2026-04-16 12:19:54 +0300

    Editing admin fixes

--- Codes/backend/MakeAdmin.php
+++ Codes/backend/MakeAdmin.php
@@ -1,12 +1,11 @@
 <?php
 include("database.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["prof_file_nb"])) {
     $prof_file_nb = $_POST["prof_file_nb"];
 
     $sql = "UPDATE professor SET isAdmin = 1 WHERE prof_file_nb = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $prof_file_nb);
+    mysqli_stmt_bind_param($stmt, "s", $prof_file_nb);
     mysqli_stmt_execute($stmt);
 
     if (mysqli_stmt_affected_rows($stmt) > 0) {
@@ -16,9 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["prof_file_nb"])) {
     }
 
     mysqli_stmt_close($stmt);
-} else {
-    echo "Invalid request.";
-}
+
 
 mysqli_close($conn);
-?>
+
