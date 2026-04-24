a48bfd0b842d7590ab8ad3b33b742bed7bb3699e
jeanCh05 <jeanchamoun93@gmail.com>
2026-04-24 16:57:44 +0300

    Update generateCode.php

--- Codes/backend/generateCode.php
+++ Codes/backend/generateCode.php
@@ -32,7 +32,7 @@
                 $mail = require __DIR__ . "/mailer.php";
 
                 $mail->setFrom("noreply@example.com");
-                $mail->addAddress("charbel.otk@gmail.com");
+                $mail->addAddress("receiveralaf@gmail.com");
                 // $mail->addAddress($email)
                 $mail->Subject = "Password Reset";
                 $mail->Body = <<<END
